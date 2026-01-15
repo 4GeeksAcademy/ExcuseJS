9c8659166ee4e3f63ac820f775b10d66181d694d
Jin Beasley <jin@ginsenglab.org>
2026-01-15 01:20:48 +0000

    Final version

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  
+  const p =  document.getElementById("excuse");
   console.log("Hello Rigo from the console!");
+
+  let who = ['My albino lizard', 'Dwayne "The Rock" Johnson', 'Chuck Norris', 'My friend from discord'];
+  let action = ['did a sick flip', 'blew up the pentagon', 'ripped his arm off', 'broke his elbow'];
+  let what = ['I completely', 'I'];
+  let when = ['lost my mind', 'was daydreaming about scarlett johannason or however you spell her name', 'pissed on myself'];
+
+  function getRandomIndex(array){
+    const length = array.length;
+    const result = Math.floor(Math.random() * length);
+    return result;
+  }
+
+  p.textContent= who[getRandomIndex(who)] + " " + action[getRandomIndex(action)]+ ", " + what[getRandomIndex(what)] + " " + when[getRandomIndex(when)];
 };
